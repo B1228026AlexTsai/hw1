a57ea4018e1cdc4175cc72d460742fee68fce4b2
Alex Tsai <alex93.tsai@gmail.com>
2025-03-09 22:08:49 +0800

    Update main4.c

--- main4.c
+++ main4.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <string.h>
 
+int main(){
+    char a[] = "Hello";
+    char temp ;
+    for (int i = 0; i < strlen(a) / 2; i++ ){
+        temp = a[i];
+        a[i] = a[strlen(a) - 1 -i];
+        a[strlen(a) - 1 -i] = temp;
+    }
+    for (int i = 0; i < strlen(a); i++){
+        printf("%c",a[i]);
+    }
+    printf("\n");
+    return 0;
+}
