b1d5a07a1eea70db8691e922e01a5e04a0e44464
Alex Tsai <alex93.tsai@gmail.com>
2025-03-10 18:32:15 +0800

    Update main2.c

--- main2.c
+++ main2.c
@@ -6,7 +6,6 @@ int main()
 {
     char a[] = "A4B1C3f3";
     int length = strlen(a);
-    int count[52] = {0};
     for (int i = 0; i < length; i += 2){
         if ('A' <= a[i] && a[i] <= 'Z'){
             for (int j = 0; j < a[i+1] - '0'; j++){
