e6f15d2cce0f81d668719b5346a85385cb72f1d5
Alex Tsai <alex93.tsai@gmail.com>
2025-03-10 18:39:47 +0800

    Update main1.c

--- main1.c
+++ main1.c
@@ -3,31 +3,21 @@
 
 int main() {
     char a[] = "AABBBCCCCddd";
-    int count[52] = {0};  // 大小寫字母各 26 個
+    int count = 1;  
 
-    // 計算字母出現次數
+    
     int length = strlen(a);
     for (int i = 0; i < length; i++) {
-        if ('A' <= a[i] && a[i] <= 'Z') {
-            count[a[i] - 'A'] += 1;  // 記錄大寫字母
-        } else if ('a' <= a[i] && a[i] <= 'z') {
-            count[a[i] - 'a' + 26] += 1;  // 記錄小寫字母
+        if (a[i] == a[i+1]) {
+            count += 1;  
+        }
+        else{
+            printf("%C%d",a[i],count);
+            count = 1;
         }
     }
 
-    // 輸出 A2B3C4d3
-    for (int i = 0; i < 26; i++) {
-        if (count[i] > 0) {
-            printf("%c%d", 'A' + i, count[i]); // 正確輸出格式
-        }
-    }
-    for (int i = 26; i < 52; i++) {
-        if (count[i] > 0) {
-            printf("%c%d", 'a' + (i - 26), count[i]); // 正確輸出格式
-        }
-    }
-    
-    printf("\n"); // 確保輸出換行
+    printf("\n"); 
     return 0;
 }
 
