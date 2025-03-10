9a7b82a5a48c3d1e54bc7f7a116e28737ddf7d69
Alex Tsai <alex93.tsai@gmail.com>
2025-03-10 18:30:56 +0800

    Update main2.c

--- main2.c
+++ main2.c
@@ -9,26 +9,17 @@ int main()
     int count[52] = {0};
     for (int i = 0; i < length; i += 2){
         if ('A' <= a[i] && a[i] <= 'Z'){
-            count[a[i] - 'A'] += a[i+1] - '0'; 
+            for (int j = 0; j < a[i+1] - '0'; j++){
+                printf("%c",a[i]);
+            }
         }
         if ('a' <= a[i] && a[i] <= 'z'){
-            count[a[i] - 'a' + 26] += a[i+1] - '0';
-        }
-    }
-    for (int i=0;i<26;i++){
-        if (count[i] > 0){
-            for (int j=0;j < count[i]; j++){
-                printf("%c",'A'+ i);
+            for (int j = 0; j < a[i+1] - '0'; j++){
+                printf("%c",a[i]);
             }
         }
     }
-    for (int i=26;i<52;i++){
-        if (count[i] > 0){
-            for (int j=0;j < count[i]; j++){
-                printf("%c",'a'+ (i - 26));
-            }
-        }
-    }
+    
     printf("\n");
 
     return 0;
